0cdf579e9aa5121a0c98ee2832f87d4a20382151
Alysa Radjac <141814931+alysaradjac@users.noreply.github.com>
2025-05-14 19:18:29 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import DashboardPage from './components/DashboardPage';
+import Notifications from './components/Notifications';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/notifications" element={<Notifications />} />
       </Routes>
     </Router>
   );
